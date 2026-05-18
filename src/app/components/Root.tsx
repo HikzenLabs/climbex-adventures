3e3351a1ac206884632cde280e83165a7bfe283e
rashid geelani <syed.geelani@vit.edu.in>
2026-05-18 06:04:37 +0530

    Scroll to top function

--- src/app/components/Root.tsx
+++ src/app/components/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import ScrollToTop from "./Scroll/ScrollToTop";
 
 export function Root() {
   return (
     <div className="min-h-screen bg-[#071018] text-white">
 
+      <ScrollToTop /> 
       <Navbar />
 
       <Outlet />
