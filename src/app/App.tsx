327062e449d54048545b2998c32bab46b9248520
rashid geelani <syed.geelani@vit.edu.in>
2026-08-08 01:32:38 +0530

    error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter, RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { WhatsAppPopup } from "./components/Home/WhatsappPopUp";
 
+
 function App() {
 
 
-  
+
   return (
     <>
       <RouterProvider router={router} />
