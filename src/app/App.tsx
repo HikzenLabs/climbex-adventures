4f41dbb090769661ae40c447961bb42b966eb512
Hikzen Labs <hikzenlabs@gmail.com>
2026-08-08 02:23:59 +0530

    Fixingproduction (#13)
    * about team
    
    * featured travels and whatsapp cta
    
    * chnagesinwhatsapppopup
    
    * error
    
    * fixedproductionbug
    
    * fix
    
    ---------
    
    Co-authored-by: rashid geelani <syed.geelani@vit.edu.in>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, RouterProvider } from "react-router-dom";
 
 import { router } from "./routes";
-import { WhatsAppPopup } from "./components/Home/WhatsappPopUp";
+import { WhatsAppPopup } from "./components/Home/WhatsAppPopup";
+
 
 
 function App() {
